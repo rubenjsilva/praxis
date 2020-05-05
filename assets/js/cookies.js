c34d017059f37a58021d7d8f63a1240406228cc1
Rúben Silva <ruben.silva@grama.io>
2020-05-06 00:55:12 +0100

    Delete log

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,7 +1,6 @@
 if(localStorage.getItem('cookieSeen') != 'shown'){
     $(".cookie-banner").delay(1000).fadeIn();
     localStorage.setItem('cookieSeen','shown')
-    console.log(cookieSeen);
 }
 
 $('.close-cookie').click(function(e) {
